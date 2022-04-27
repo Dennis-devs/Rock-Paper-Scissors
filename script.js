62aa96a009f6bd477158107244483d0be42835dd
Dennis-devs <dennis.ogoll@gmail.com>
2022-04-27 08:58:41 +0300

    Write a function rounds in script.js that takes two parameters and returns a string that declares the winner. Make the playerSelection parameter casse insensitive

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
 const choices = ["rock", "paper", "scissor"];
-var op1 = "SCISSOR";
-var op2 = "Rock"
-var op3 = "Paper";
+// var op1 = "SCISSOR";
+// var op2 = "Rock"
+// var op3 = "Paper";
+
+function userPlay(){
+let userInput = prompt("Enter rock, paper or scissor");
+let num = choices.indexOf(userInput.toLowerCase());
+if (num > -1){
+    console.log(choices[num])
+    return choices[num]                                
+}                                                  
+}
 
 function computerPlay(){
-    var random = Math.floor(Math.random() * choices.length)
+    let random = Math.floor(Math.random() * choices.length)
     console.log(choices[random])
     return choices[random]
 }
 
-function userPlay(){
-//var userInput = prompt("Enter rock, paper or scissor");
-var num = choices.indexOf(op1.toLowerCase());
-if (num > -1){
-    console.log(choices[num])
-    return choices[num]
-} 
-}
-
-var computerSelection = computerPlay()
-var playerSelection = userPlay()
+let computerSelection = computerPlay()
+let playerSelection = userPlay()
 
 function rounds(playerSelection, computerSelection) {
-    if (playerSelection === "scissor" && computerSelection === "paper") {
-          return "you win! scissor beats paper";
+    if(playerSelection === computerSelection){
+        return "Its a tie!"
+    } 
+    else if(playerSelection === "scissor" && computerSelection === "paper") {
+        return "You Win! scissor beats paper";
     }
     else if(playerSelection === "scissor" && computerSelection === "rock"){
-    return "you loose! rock beats scissor"
+        return "You Loose! rock beats scissor"
     }
-    else if (playerSelection === "scissor" && computerSelection === "scissor"){
-    return "Its a Tie!"
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You Loose! paper beats rock"
     }
-    else return "Player, enter correct values"                
+    else if (playerSelection === "rock" && computerSelection === "scissor"){
+        return "You Win! rock bets scissor"
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You Win! paper beats rock"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "You Loose! scissor beats paper"
+    }
+    else return "Player, Enter correct values!"                
  }
-console.log(rounds(playerSelection, computerSelection))
+ 
+
+// function game(){
+//     let playerWins = []
+//     let computerWins = []
+//     for (let i = 1; i <= 5 ; i++) {
+  
+//         if (rounds(playerSelection, computerSelection) === "Its a tie!") {
+//             return "round tied"
+//         }
+//         else if(rounds(playerSelection, computerSelection) === "You Win! scissor beats paper" || rounds(playerSelection, computerSelection) === "You Win! rock bets scissor" || rounds(playerSelection, computerSelection) === "You Win! paper beats rock") {
+//             playerWins.push(i)
+//             console.log(playerWins)
+//             return "You win round this round"
+//         } 
+//         else if(rounds(playerSelection, computerSelection) === "You Loose! rock beats scissor" || rounds(playerSelection, computerSelection) === "You Loose! paper beats rock" || rounds(playerSelection, computerSelection) === "You Loose! scissor beats paper") {
+//             computerWins.push(i)
+//             console.log(computerWins)
+//             return "Computer wins this round"
+//         }  
+    
+//     }
+//     if (playerWins.length > computerWins.length) {
+//         return "you are the champion"
+//     }
+//     else if(computerWins > playerWins) {
+//         return "computer is the champion"
+//     }
+//     else return "Its a 5 round tie"
+// }
+//function game()
 
 
 
